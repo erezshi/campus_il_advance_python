e85706f49e48e84342868f3c0eaeaa138ce03c8b
erezshi <erez.shi@gmail.com>
2021-01-25 10:45:28 +0200

    Update ex_5.4.py

--- iterators/ex_5.4.py
+++ iterators/ex_5.4.py
@@ -13,6 +13,16 @@ class IDIterator:
     def __init__(self, id=100000000):
         self._id = id
         # self.id_index = -1
+         
+    def check_id_valid(self, id_number):
+        id_multiple = [ int(n[1]) * 2 if n[0] % 2 == 0 else int(n[1]) for n in enumerate(str(id_number), start=1) ]
+        id_multiple = list(map(str, id_multiple)) # back to str
+        id_plus = [ int(n[0]) + int(n[1]) if len(n) > 1 else int(n) for n in id_multiple ] 
+        sum_id = reduce(lambda x, y: x + y, id_plus) # just for fun I used it instead of sum function
+        if sum_id % 10 == 0:
+            return True
+        else:
+            return False
 
     def __iter__(self):
         return self
